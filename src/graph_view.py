ff9e9bb45e366004df01d8291b820ed36c3fd997
wg_andrey <diazonium@mail.ru>
2018-11-05 16:15:23 +0300

    Removed pyplot dependence for visualization

--- src/graph_view.py
+++ src/graph_view.py
@@ -1,23 +1,33 @@
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
+import tkinter as tk
 import networkx as nx
-
-
-def _draw(canvas, G):
-    plt.clf()
-    nx.draw(G, layout=nx.spring_layout(G))
-    canvas.draw()
 
 
 class GraphViewer:
     def __init__(self, parent):
-        f = plt.figure(figsize=(5, 4), dpi=100)
         self._parent = parent
-        self._canvas = FigureCanvasTkAgg(f, master=parent)
-        self._canvas.get_tk_widget().pack()
+        self._canvas = tk.Canvas(parent, width=1024, height=768)
+        self._canvas.pack()
 
     def on_next(self, G):
-        _draw(self._canvas, G)
+        self._canvas.delete('all')
+
+        def scale(x, y):
+            zoom = 0.7
+            width = self._canvas.winfo_width()
+            height = self._canvas.winfo_height()
+            size = min(width, height)
+            x = (width + size * x * zoom) / 2
+            y = (height + size * y * zoom) / 2
+            return x, y
+
+        layout = nx.spring_layout(G)
+        for from_, to in G.edges:
+            x_from, y_from = scale(*layout[from_])
+            x_to, y_to = scale(*layout[to])
+            self._canvas.create_line(x_from, y_from, x_to, y_to)
+        for x, y in layout.values():
+            x, y = scale(x, y)
+            self._canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill='red')
 
     def on_error(self, err):
         print('error in drawing', err)
