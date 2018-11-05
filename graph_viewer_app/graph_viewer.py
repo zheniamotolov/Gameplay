5c960b061a1870976b8e36794cce89abaa1f0cc4
Eugene Motolov <zheniamotolov@gmail.com>
2018-11-05 22:06:50 +0300

    Fix bug with import

--- graph_viewer_app/graph_viewer.py
+++ graph_viewer_app/graph_viewer.py
@@ -10,32 +10,37 @@ import pygraphviz as pgv
 class GraphViewerGUI:
     def __init__(self, parent):
         self._graph = nx.Graph()
-        figure = plt.figure(figsize=(5, 4), dpi=100)
+        figure = plt.figure(figsize=(10, 10))
         self._parent = parent
         self._canvas = FigureCanvasTkAgg(figure, master=parent)
         self._canvas.get_tk_widget().pack()
         self._parent.wm_protocol('WM_DELETE_WINDOW', parent.quit)
         self.create_menubar()
-        self.create_buttons()
 
     def create_menubar(self):
         menubar = tk.Menu(self._parent)
-        filemenu = tk.Menu(menubar)
-        import_handler = ImportHandler(self._graph)
+        files_menubar = tk.Menu(menubar, tearoff=0)
+        import_handler = ImportHandler()
         export_handler = ExportHandler()
-        filemenu.add_command(label='Import', command=lambda: self.draw(import_handler.import_graph_from_file()))
-        filemenu.add_command(label='Export',
-                             command=lambda: self.draw(export_handler.export_graph_to_file(self._graph)))
-        filemenu.add_command(label='Visualize planar embedding of graph',
-                             command=lambda: self.show_planar_embedding_of_graph())
-        menubar.add_cascade(label='File', menu=filemenu)
+        files_menubar.add_command(label='Import',
+                                  command=lambda: self.draw(import_handler.import_graph_from_file(self.reset_graph())))
+        files_menubar.add_command(label='Export',
+                                  command=lambda: self.draw(export_handler.export_graph_to_file(self._graph)))
+        files_menubar.add_command(label='Visualize planar embedding of graph',
+                                  command=lambda: self.show_planar_embedding_of_graph())
+        menubar.add_cascade(label='File', menu=files_menubar)
+
+        grphs_menubar = tk.Menu(menubar, tearoff=0)
+        grphs_menubar.add_command(label='Cube', command=lambda: self.show_cube())
+        grphs_menubar.add_command(label='Cycle', command=lambda: self.show_cycle())
+        grphs_menubar.add_command(label='Complete', command=lambda: self.show_complete())
+        grphs_menubar.add_command(label='4D hypercube', command=lambda: self.show_hyper4d())
+        menubar.add_cascade(label='Graphs', menu=grphs_menubar)
         self._parent.config(menu=menubar)
 
-    def create_buttons(self):
-        tk.Button(self._parent, text='Cube', command=lambda: self.show_cube()).pack(side='left', padx=(130, 0))
-        tk.Button(self._parent, text='Cycle', command=lambda: self.show_cycle()).pack(side='left')
-        tk.Button(self._parent, text='Complete', command=lambda: self.show_complete()).pack(side='left')
-        tk.Button(self._parent, text='4D hypercube', command=lambda: self.show_hyper4d()).pack(side='left')
+    def reset_graph(self):
+        self._graph = nx.Graph()
+        return self._graph
 
     def show_complete(self):
         self._graph = nx.complete_graph(5)
@@ -55,16 +60,17 @@ class GraphViewerGUI:
         self.draw(self._graph, show_labels)
 
     def show_planar_embedding_of_graph(self):
-        if nx.check_planarity(self._graph)[0]:
-            a_graph = pgv.AGraph()
-            a_graph.node_attr['shape'] = 'circle'
-            a_graph.node_attr['style'] = 'filled'
-            a_graph.node_attr['color'] = 'red'
-            a_graph.add_edges_from(self._graph.edges())
-            a_graph.layout(prog='dot')
-            a_graph.draw('../resources/planar.png')
-        else:
-            print('graph in not planar')
+        # if nx.check_planarity(self._graph)[0]:
+        a_graph = pgv.AGraph()
+        a_graph.node_attr['shape'] = 'circle'
+        a_graph.node_attr['style'] = 'filled'
+        a_graph.node_attr['color'] = 'red'
+        a_graph.add_edges_from(self._graph.edges())
+        a_graph.layout(prog='dot')
+        a_graph.draw('../resources/planar.png')
+
+    # else:
+    #     print('graph is not planar')
 
     def draw(self, graph, show_labels=True):
         plt.clf()
--- graph_viewer_app/import_handler.py
+++ graph_viewer_app/import_handler.py
@@ -2,21 +2,20 @@ from tkinter import filedialog
 import json
 
 
-class ImportHandler:
-    def __init__(self, graph):
-        self._graph = graph
 
-    def import_graph_from_file(self):
+class ImportHandler:
+
+    def import_graph_from_file(self, graph):
         filetypes = [('JSON', '*.json'), ('All Files', '*')]
         filename = filedialog.askopenfile(filetypes=filetypes).name
-        return self.read_graph(filename)
+        return self.read_graph(filename, graph)
 
-    def read_graph(self, filename):
+    def read_graph(self, filename, graph):
         with open(filename, 'r') as fp:
             data = json.load(fp)
         for point in data['points']:
-            self._graph.add_node(point['idx'])
+            graph.add_node(point['idx'])
         for line in data['lines']:
             from_idx, to_idx = line['points']
-            self._graph.add_edge(from_idx, to_idx, weight=line['length'])
-        return self._graph
+            graph.add_edge(from_idx, to_idx, weight=line['length'])
+        return graph
