75f8741a7a617da98e396c41c07db6f38294e9da
Vladislav Lukyanov <uladzislau.luk@gmail.com>
2018-11-07 16:21:09 +0300

    Moved GUI construction and data bindings to separate classes
    Created more like MVC style separation, where View deals with visual
    part of an app, Model - data, Controller is the main interaction part
    which couples View and Model on construction.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,9 @@
-import tkinter as tk
-import networkx as nx
-from rx.subjects import BehaviorSubject
-from graph_view import GraphViewer
-import menus
+from app import AppController
 
 
 def main():
-    G = BehaviorSubject(nx.Graph())
-    root = tk.Tk()
-    graph_viewer = GraphViewer(root)
-    G.subscribe(graph_viewer)
-    root.wm_protocol('WM_DELETE_WINDOW', root.quit)
-    menubar = tk.Menu(root, tearoff=0)
-    menus.file(menubar, G)
-    menus.view(menubar, graph_viewer)
-    menus.graphs(menubar, G)
-    menus.layouts(menubar, graph_viewer)
-    root.config(menu=menubar)
-    tk.mainloop()
+    controller = AppController()
+    controller.run()
 
 
 if __name__ == '__main__':
