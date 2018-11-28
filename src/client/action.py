f3c9362c917f7ec7111f3d95cb3d85ef2fc9b5d1
Uladzislau Lukyanau <32821684+uladzislau-luk@users.noreply.github.com>
2018-11-28 10:22:44 +0300

    Removed no longer necessary files

--- src/client/action.py
+++
@@ -1,8 +0,0 @@
-class Action:
-    COMMAND = {
-        1: "LOGIN",
-        2: 'LOGOUT',
-        3: 'MOVE',
-        4: 'UPDATE',
-        5: 'TURN',
-        10: 'MAP'}
--- src/client/config.py
+++
@@ -1,4 +0,0 @@
-HOST_NAME = "wgforge-srv.wargaming.net"
-PORT = 443
-USERNAME="johne"
-PASSWORD="1123"
